934938bb549c98e678f70a89011e8b89534d54fb
Zaina15 <136789779+Zaina15@users.noreply.github.com>
2025-04-26 10:38:59 -0700

    stuff

---
+++ database/app.py
@@ -0,0 +1,19 @@
+# app.py
+from flask import Flask
+from db import db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///roomieconnect.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return 'Welcome to RoomieConnect!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
